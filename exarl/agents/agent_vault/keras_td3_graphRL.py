bug fixes with invalid action masking in space

exarl/agents/agent_vault/keras_td3_graphRL.py
@@ -90,7 +90,7 @@ def get_graph_adj(knownStates, state):
 class KerasGraphTD3RL(exarl.ExaAgent):
 
     def __init__(self, env, is_learner, **kwargs):
-        """ Define all key variables required for all agent """
+        """ Define all key variables required for all agent """ 
 
         self.is_learner = is_learner
         # Get env info
@@ -151,7 +151,8 @@ class KerasGraphTD3RL(exarl.ExaAgent):
         # update counting
         self.ntrain_calls = 0
         self.actor_update_freq = 2
-        self.critic_update_freq = 2
+        self.target_critic_update_freq = 2
+        self.critic_update_freq = 1
 
         # Not used by agent but required by the learner class
         self.epsilon = ExaGlobals.lookup_params('epsilon')
@@ -166,25 +167,20 @@ class KerasGraphTD3RL(exarl.ExaAgent):
         logger().info("TD3 actor_lr {}".format(actor_lr))
 
     @tf.function
-    def train_critic(self, states, actions, rewards, next_states, masks, masks_next):
-        # states = states[0]
-        # next_states = next_states[0]
+    def train_critic(self, states, actions, rewards, next_states, masks, next_masks):
         next_actions = self.target_actor(next_states, training=False)
 
-        # print("next_actions SHAPE: ", next_actions.shape)
-        # Add a little noise
-        noise = np.random.normal(0, 0.2, self.num_actions)
+        #Masking the invalid actions
+        next_actions = tf.where(next_masks, next_actions, tf.zeros_like(next_actions))
+
+        #Add a little noise
+        noise = np.random.normal(0, 0.1, self.node_count)
         noise = np.clip(noise, -0.5, 0.5)
         next_actions = next_actions * (1 + noise)
 
-        #Invalid action masking on next actions
-        masks_next_complete = np.zeros(next_actions.shape, dtype=int)
-        for i in range(len(masks_next)):
-                mask_indices = np.array(masks_next[i])
-                masks_next_complete[i][mask_indices] = 1
+        next_actions = tf.nn.softmax(next_actions)
+        # next_actions = tf.random.categorical(sampled_actions_probs, self.num_actions)
         
-        next_actions = next_actions*masks_next_complete
-
         new_q1 = self.target_critic1([next_states, next_actions], training=False)
         new_q2 = self.target_critic2([next_states, next_actions], training=False)
         new_q = tf.math.minimum(new_q1, new_q2)
@@ -209,22 +205,18 @@ class KerasGraphTD3RL(exarl.ExaAgent):
     @tf.function
     def train_actor(self, states, masks):
         # Use Critic 1
-        # states = states[0]
         with tf.GradientTape() as tape:
             actions = self.actor_model(states, training=True)
-            # print("ACTIONS SHAPE: ", actions.shape)
 
+            # Invalid action masking with tensors
+            actions = tf.where(masks, actions, tf.zeros_like(actions))
 
-            # Invalid action masking on next actions
-            masks_next_complete = np.zeros(actions.shape, dtype=int)
-            for i in range(len(masks)):
-                mask_indices = np.array(masks[i])
-                masks_next_complete[i][mask_indices] = 1
-
-            actions = actions*masks_next_complete
+            actions = tf.nn.softmax(actions)
 
             q_value = self.critic_model1([states, actions], training=True)
             loss = -tf.math.reduce_mean(q_value)
+            tf.print("Actor Training Loss: ", loss)
+
         gradient = tape.gradient(loss, self.actor_model.trainable_variables)
         self.actor_optimizer.apply_gradients(zip(gradient, self.actor_model.trainable_variables))
 
@@ -294,65 +286,59 @@ class KerasGraphTD3RL(exarl.ExaAgent):
         for (target_weight, weight) in zip(target_weights, weights):
             target_weight.assign(weight * self.tau + target_weight * (1.0 - self.tau))
 
-    def update(self, state_batch, action_batch, reward_batch, next_state_batch):
-        # print("SHAPE OF state_batch: ", state_batch.shape)
-        # print("SHAPE OF state_batch[0]: ", state_batch[0,0].shape)
-        # print("SHAPE OF state_batch[2]: ", type(state_batch[0,2]))
-        # print("SHAPE OF next_state_batch: ", next_state_batch.shape)
-        # print("Single batch: ", state_batch[0,1].shape)
+    def update(self, state_batch, action_batch, reward_batch, next_state_batch, masks, next_masks):
+        if self.ntrain_calls % self.actor_update_freq == 0:
+            self.train_actor(state_batch, masks)
+        if self.ntrain_calls % self.critic_update_freq == 0:
+            self.train_critic(state_batch, action_batch, reward_batch, next_state_batch, masks, next_masks)
         
-        tf_state_batch = np.zeros(shape=(len(state_batch[:,0]),self.adj_shape[0],self.adj_shape[1]))
-        tf_next_state_batch = np.zeros(shape=(len(next_state_batch[:,0]),self.adj_shape[0],self.adj_shape[1]))
+    def _convert_to_tensor(self, state_batch, action_batch, reward_batch, next_state_batch, terminal_batch):
         masks = []
-        masks_next = []
+        next_masks = []
 
-        for i in range(len(state_batch[:,0])):
-            tf_state_batch[i] = state_batch[i,0]
-            tf_next_state_batch[i] = tf_next_state_batch[i,0]
+        tf_state_batch = np.zeros(shape=(self.batch_size,self.adj_shape[0],self.adj_shape[1]*2))
+        tf_next_state_batch = np.zeros(shape=(self.batch_size,self.adj_shape[0],self.adj_shape[1]*2))
+        for i in range(self.batch_size):
+
             known_keys = [x for x in state_batch[i,2].keys() if state_batch[i,2][x] != None]
             known_keys_next = [x for x in next_state_batch[i,2].keys() if next_state_batch[i,2][x] != None]
-            masks.append(known_keys)
-            masks_next.append(known_keys_next)
-            print(action_batch[0])
-            print("Batch idx: ", i, " Values that don't equal zero:", sum(value != 0 for value in action_batch[i]))
 
-        tf_state_batch = tf.convert_to_tensor(tf_state_batch, dtype=tf.float32)
-        tf_next_state_batch = tf.convert_to_tensor(tf_next_state_batch, dtype=tf.float32)
+            mask_indices = np.array(known_keys)
+            next_mask_indices = np.array(known_keys_next)
 
+            mask = np.full(self.node_count, False)
+            mask[mask_indices] = True
+
+            next_mask = np.full(self.node_count, False)
+            next_mask[next_mask_indices] = True
+
+            masks.append(mask)
+            next_masks.append(next_mask)
+
+            tf_state_batch[i] = tf.convert_to_tensor(state_batch[i,0], dtype=tf.float32)
+            tf_next_state_batch[i] = tf.convert_to_tensor(next_state_batch[i,0], dtype=tf.float32)
         
-
-        self.train_critic(tf_state_batch, action_batch, reward_batch, tf_next_state_batch, masks, masks_next)
-        self.train_actor(tf_state_batch, masks)
-
-    def _convert_to_tensor(self, state_batch, action_batch, reward_batch, next_state_batch, terminal_batch):
-        for i in range(len(state_batch[:,0])):
-            state_batch[i,0] = tf.convert_to_tensor(state_batch[i,0], dtype=tf.float32)
-        # state_batch = tf.convert_to_tensor(state_batch[:,0], dtype=tf.float32)
         action_batch = tf.convert_to_tensor(action_batch, dtype=tf.float32)
         reward_batch = tf.convert_to_tensor(reward_batch, dtype=tf.float32)
-        for i in range(len(next_state_batch[:,0])):
-            next_state_batch[i,0] = tf.convert_to_tensor(next_state_batch[i,0], dtype=tf.float32)
-        # next_state_batch[:,0] = tf.convert_to_tensor(next_state_batch[:,0], dtype=tf.float32)
         terminal_batch = tf.convert_to_tensor(terminal_batch, dtype=tf.float32)
-        return state_batch, action_batch, reward_batch, next_state_batch, terminal_batch
+        return tf_state_batch, action_batch, reward_batch, tf_next_state_batch, terminal_batch, masks, next_masks
 
     def generate_data(self):
-        state_batch, action_batch, reward_batch, next_state_batch, done_batch = \
+        state_batch, action_batch, reward_batch, next_state_batch, done_batch, masks_batch, next_masks_batch = \
             self._convert_to_tensor(*self.memory.sample_buffer(self.batch_size))
-        yield state_batch, action_batch, reward_batch, next_state_batch, done_batch
+        yield state_batch, action_batch, reward_batch, next_state_batch, done_batch, masks_batch, next_masks_batch
 
     def train(self, batch):
         """ Method used to train """
         self.ntrain_calls += 1
-        self.update(batch[0], batch[1], batch[2], batch[3])
+        self.update(batch[0], batch[1], batch[2], batch[3], batch[5], batch[6])
 
     def update_target(self):
         if self.ntrain_calls % self.actor_update_freq == 0:
             self.soft_update(self.target_actor.variables, self.actor_model.variables)
-        if self.ntrain_calls % self.critic_update_freq == 0:
+        if self.ntrain_calls % self.target_critic_update_freq == 0:
             self.soft_update(self.target_critic1.variables, self.critic_model1.variables)
             self.soft_update(self.target_critic2.variables, self.critic_model2.variables)
-
 
     def action(self, state):
         taskList = []
@@ -375,15 +361,9 @@ class KerasGraphTD3RL(exarl.ExaAgent):
     
     def mini_action(self, state, known_keys):
         """ Method used to provide the next action using the target model """
-        # print("State: ", state)
-        # dictator = state[2]
-        # state = state[0]
-        
-        # print("ACTOR CHECK: ", dictator[5050])
 
         tf_state = tf.expand_dims(tf.convert_to_tensor(state), 0)
         sampled_actions = tf.squeeze(self.actor_model(tf_state))
-        # print("Sampled action output from the actor: ", sampled_actions)
 
         noise = np.random.normal(0, 0.1, self.num_actions)
         sampled_actions = sampled_actions.numpy() * (1 + noise)

exarl/agents/agent_vault/keras_td3_graphRL_space.py
@@ -137,23 +137,28 @@ class KerasGraphTD3RLSpace(exarl.ExaAgent):
         
         next_adj_mat, next_dat_mat = tf.split(next_states, num_or_size_splits=2, axis=2)
         next_actions = self.target_actor([next_adj_mat, next_dat_mat], training=False)
-
+        
+        # Add a little noise
+        noise = self.ou_noise()
+        next_actions = next_actions + noise
+        # noise = np.random.normal(0, 0.1, self.node_count)
+        # noise = np.clip(noise, -0.5, 0.5)
+        # next_actions = next_actions * (1 + noise)
+       
         #Masking the invalid actions
-        next_actions = tf.where(next_masks, next_actions, tf.zeros_like(next_actions))
-
-        #Add a little noise
-        noise = np.random.normal(0, 0.1, self.node_count)
-        noise = np.clip(noise, -0.5, 0.5)
-        next_actions = next_actions * (1 + noise)
-
+        next_actions = tf.where(next_masks, next_actions, tf.constant(-np.inf, shape=next_actions.shape))
         next_actions = tf.nn.softmax(next_actions)
-        # next_actions = tf.random.categorical(sampled_actions_probs, self.num_actions)
         
         new_q1 = self.target_critic1([next_adj_mat, next_dat_mat, next_actions], training=False)
         new_q2 = self.target_critic2([next_adj_mat, next_dat_mat, next_actions], training=False)
         new_q = tf.math.minimum(new_q1, new_q2)
+
         # Bellman equation for the q value
         q_targets = rewards + self.gamma * new_q
+
+        actions = tf.where(masks, actions, tf.constant(-np.inf, shape=actions.shape))
+        actions = tf.nn.softmax(actions)
+
         # Critic 1
         with tf.GradientTape() as tape:
             adj_mat, dat_mat = tf.split(states, num_or_size_splits=2, axis=2)
@@ -179,20 +184,13 @@ class KerasGraphTD3RLSpace(exarl.ExaAgent):
             adj_mat, dat_mat = tf.split(states, num_or_size_splits=2, axis=2)
 
             actions = self.actor_model([adj_mat, dat_mat], training=True)
-            # print("ACTIONS: ", actions)
 
             # Invalid action masking with tensors
-            actions = tf.where(masks, actions, tf.zeros_like(actions))
-
+            actions = tf.where(masks, actions, tf.constant(-np.inf, shape=actions.shape))
             actions = tf.nn.softmax(actions)
-            # print("sampled_actions_probs: ", sampled_actions_probs)
 
-            # actions = tf.random.categorical(sampled_actions_probs, self.num_actions)
-            # print("ACTIONS: ", actions)
             q_value = self.critic_model1([adj_mat, dat_mat, actions], training=True)
-            # print("q_value: ", q_value)
             loss = -tf.math.reduce_mean(q_value)
-            tf.print("Actor Training Loss: ", loss)
 
         gradient = tape.gradient(loss, self.actor_model.trainable_variables)
         # print(gradient)
@@ -225,7 +223,7 @@ class KerasGraphTD3RLSpace(exarl.ExaAgent):
         # action_out = tf.keras.layers.Dense(self.num_actions / 1000, activation="relu", name='action_4')(action_out)
 
         # Both are passed through separate layer before concatenating
-        concat = tf.keras.layers.Add()([state_out, action_out])
+        concat = tf.keras.layers.Concatenate()([state_out, action_out])
 
         # out = tf.keras.layers.Dense(1024, activation="relu")(concat)
         # out = tf.keras.layers.Dense(512, activation="relu")(concat)
@@ -276,8 +274,8 @@ class KerasGraphTD3RLSpace(exarl.ExaAgent):
                                         use_bias=True)(out)
 
         # Rescale for tanh [-1,1]
-        # outputs = tf.keras.layers.Lambda(
-        #     lambda x: ((x + 1.0) * (self.upper_bound - self.lower_bound)) / 2.0 + self.lower_bound)(outputs)
+        outputs = tf.keras.layers.Lambda(
+            lambda x: ((x + 1.0) * (self.upper_bound - self.lower_bound)) / 2.0 + self.lower_bound)(outputs)
 
         model = tf.keras.Model([adj_inputs, dat_inputs], outputs)
         model.summary()
@@ -294,7 +292,6 @@ class KerasGraphTD3RLSpace(exarl.ExaAgent):
         if self.ntrain_calls % self.critic_update_freq == 0:
             self.train_critic(state_batch, action_batch, reward_batch, next_state_batch, masks, next_masks)
         
-
     def _convert_to_tensor(self, state_batch, action_batch, reward_batch, next_state_batch, terminal_batch):
         masks = []
         next_masks = []

exarl/agents/agent_vault/keras_td3_graphRL_task.py
@@ -165,23 +165,17 @@ class KerasGraphTD3RLTask(exarl.ExaAgent):
         # Use Critic 1
         with tf.GradientTape() as tape:
             actions = self.actor_model(states, training=True)
-            # print("ACTIONS: ", actions)
 
             # Invalid action masking with tensors
             actions = tf.where(masks, actions, tf.zeros_like(actions))
 
             actions = tf.nn.softmax(actions)
-            # print("sampled_actions_probs: ", sampled_actions_probs)
 
-            # actions = tf.random.categorical(sampled_actions_probs, self.num_actions)
-            # print("ACTIONS: ", actions)
             q_value = self.critic_model1([states, actions], training=True)
-            # print("q_value: ", q_value)
             loss = -tf.math.reduce_mean(q_value)
             tf.print("Actor Training Loss: ", loss)
 
         gradient = tape.gradient(loss, self.actor_model.trainable_variables)
-        # print(gradient)
         self.actor_optimizer.apply_gradients(zip(gradient, self.actor_model.trainable_variables))
 
     def get_critic(self):
@@ -253,21 +247,20 @@ class KerasGraphTD3RLTask(exarl.ExaAgent):
         for (target_weight, weight) in zip(target_weights, weights):
             target_weight.assign(weight * self.tau + target_weight * (1.0 - self.tau))
 
-    def update(self, state_batch, action_batch, reward_batch, next_state_batch):
-        # print("SHAPE OF state_batch: ", state_batch.shape)
-        # print("SHAPE OF state_batch[0]: ", state_batch[0,0].shape)
-        # print("SHAPE OF state_batch[2]: ", type(state_batch[0,2]))
-        # print("SHAPE OF next_state_batch: ", next_state_batch.shape)
-        # print("Single batch: ", state_batch[0,1].shape)
+    def update(self, state_batch, action_batch, reward_batch, next_state_batch, masks, next_masks):
+        if self.ntrain_calls % self.actor_update_freq == 0:
+            self.train_actor(state_batch, masks)
+        if self.ntrain_calls % self.critic_update_freq == 0:
+            self.train_critic(state_batch, action_batch, reward_batch, next_state_batch, masks, next_masks)
         
-        tf_state_batch = np.zeros(shape=(len(state_batch[:,0]),self.adj_shape[0],self.adj_shape[1]))
-        tf_next_state_batch = np.zeros(shape=(len(next_state_batch[:,0]),self.adj_shape[0],self.adj_shape[1]))
+
+    def _convert_to_tensor(self, state_batch, action_batch, reward_batch, next_state_batch, terminal_batch):
         masks = []
         next_masks = []
 
-        for i in range(len(state_batch[:,0])):
-            tf_state_batch[i] = state_batch[i,0]
-            tf_next_state_batch[i] = tf_next_state_batch[i,0]
+        tf_state_batch = np.zeros(shape=(self.batch_size,self.adj_shape[0],self.adj_shape[1]))
+        tf_next_state_batch = np.zeros(shape=(self.batch_size,self.adj_shape[0],self.adj_shape[1]))
+        for i in range(self.batch_size):
 
             known_keys = [x for x in state_batch[i,2].keys() if state_batch[i,2][x] != None]
             known_keys_next = [x for x in next_state_batch[i,2].keys() if next_state_batch[i,2][x] != None]
@@ -284,33 +277,23 @@ class KerasGraphTD3RLTask(exarl.ExaAgent):
             masks.append(mask)
             next_masks.append(next_mask)
 
-        tf_state_batch = tf.convert_to_tensor(tf_state_batch, dtype=tf.float32)
-        tf_next_state_batch = tf.convert_to_tensor(tf_next_state_batch, dtype=tf.float32)
-
-        self.train_critic(tf_state_batch, action_batch, reward_batch, tf_next_state_batch, masks, next_masks)
-        self.train_actor(tf_state_batch, masks)
-
-    def _convert_to_tensor(self, state_batch, action_batch, reward_batch, next_state_batch, terminal_batch):
-        for i in range(len(state_batch[:,0])):
-            state_batch[i,0] = tf.convert_to_tensor(state_batch[i,0], dtype=tf.float32)
-        # state_batch = tf.convert_to_tensor(state_batch[:,0], dtype=tf.float32)
+            tf_state_batch[i] = tf.convert_to_tensor(state_batch[i,0], dtype=tf.float32)
+            tf_next_state_batch[i] = tf.convert_to_tensor(next_state_batch[i,0], dtype=tf.float32)
+        
         action_batch = tf.convert_to_tensor(action_batch, dtype=tf.float32)
         reward_batch = tf.convert_to_tensor(reward_batch, dtype=tf.float32)
-        for i in range(len(next_state_batch[:,0])):
-            next_state_batch[i,0] = tf.convert_to_tensor(next_state_batch[i,0], dtype=tf.float32)
-        # next_state_batch[:,0] = tf.convert_to_tensor(next_state_batch[:,0], dtype=tf.float32)
         terminal_batch = tf.convert_to_tensor(terminal_batch, dtype=tf.float32)
-        return state_batch, action_batch, reward_batch, next_state_batch, terminal_batch
+        return tf_state_batch, action_batch, reward_batch, tf_next_state_batch, terminal_batch, masks, next_masks
 
     def generate_data(self):
-        state_batch, action_batch, reward_batch, next_state_batch, done_batch = \
+        state_batch, action_batch, reward_batch, next_state_batch, done_batch, masks_batch, next_masks_batch = \
             self._convert_to_tensor(*self.memory.sample_buffer(self.batch_size))
-        yield state_batch, action_batch, reward_batch, next_state_batch, done_batch
+        yield state_batch, action_batch, reward_batch, next_state_batch, done_batch, masks_batch, next_masks_batch
 
     def train(self, batch):
         """ Method used to train """
         self.ntrain_calls += 1
-        self.update(batch[0], batch[1], batch[2], batch[3])
+        self.update(batch[0], batch[1], batch[2], batch[3], batch[5], batch[6])
 
     def update_target(self):
         if self.ntrain_calls % self.actor_update_freq == 0:

exarl/agents/agent_vault/keras_td3_tuple.py
@@ -294,7 +294,9 @@ class KerasTD3Tuple(exarl.ExaAgent):
 
         tf_state = tf.expand_dims(tf.convert_to_tensor(state[0]), 0)
         
-        sampled_actions = tf.squeeze(self.actor_model(tf_state))
+        test = self.actor_model(tf_state)
+        print("Output of network: ", test)
+        sampled_actions = tf.squeeze(test)
         # noise = np.random.normal(0, 0.1, self.num_actions)
 
         # Changed the noise
@@ -305,7 +307,7 @@ class KerasTD3Tuple(exarl.ExaAgent):
         # We make sure action is within bounds
         legal_action = np.clip(sampled_actions, self.lower_bound, self.upper_bound)
         # tf.print("legal_action", legal_action.shape)
-
+        print("Legal action: ", legal_action)
         # return [np.squeeze(legal_action)], [np.squeeze(noise)]
         return [np.squeeze(legal_action)], policy_type
 

exarl/envs/env_vault/ExaExaaltGraphRLSpace.py
@@ -24,8 +24,8 @@ import gym
 from exarl.utils.globals import ExaGlobals
 from datetime import datetime
 
-run_name = 'Exaalt_GraphTD3-v3_ExaExaaltGraph-v3_AC_250nw_50sd_10kNoS_200e_100eps_PATCH_'
-
+# run_name = 'Exaalt_GraphTD3-v3_ExaExaaltGraph-v3_AC_15nw_3sd_500NoS_150e_100eps_mask_PATCH_'
+run_name = "Exaalt_GraphTD3-v3_ExaExaaltGraph-v3_AC_250nw_50sd_100KNoS_125e_100eps_"
 try:
     graph_size = ExaGlobals.lookup_params('graph_size')
 except:
@@ -206,7 +206,7 @@ class StateStatistics:
 
 class ExaExaaltGraphRLSpace(gym.Env):
 
-    metadata = {"node_count": 10000}
+    metadata = {"node_count": 100000}
 
     def __init__(self,**kwargs):
         super().__init__()
@@ -214,7 +214,7 @@ class ExaExaaltGraphRLSpace(gym.Env):
 
         """
         stateDepth       = 50 #segments
-        number_of_states = 10000
+        number_of_states = 100000
 
         self.n_states  = number_of_states
         # self.nWorkers  = 500
@@ -257,7 +257,7 @@ class ExaExaaltGraphRLSpace(gym.Env):
             self.Map[i][(i-side)%number_of_states]=R
             self.Map[i][(i+side)%number_of_states]=R
 
-        self.INITIAL_STATE              = int(((side/2)*side+side/2)/10) 
+        self.INITIAL_STATE              = int(((side/2)*side+side/2)) 
         # self.INITIAL_STATE = np.random.randint(0,self.n_states)   
         # print("INITIAL STATE: ", type(self.INITIAL_STATE))
         # print("MAP at init: ", self.Map[self.INITIAL_STATE])
@@ -458,7 +458,7 @@ class ExaExaaltGraphRLSpace(gym.Env):
 
         """ Iterates the testing process forward one step """
 
-        # reward        = 0.0*(len(self.traj)-1)/float(self.WCT*self.nWorkers) + (added/self.nWorkers)
+        # reward        = 0.5*(len(self.traj)-1)/float(self.WCT*self.nWorkers) + 0.5*(added/self.nWorkers)
         reward = (len(self.traj)-1)/float(self.WCT*self.nWorkers)
         current_state = self.traj[-1]
 
@@ -473,7 +473,7 @@ class ExaExaaltGraphRLSpace(gym.Env):
         side = 100
 
         self.WCT                             = 0 
-        self.INITIAL_STATE                   = int(((side/2)*side+side/2)/10)
+        self.INITIAL_STATE                   = int(((side/2)*side+side/2))
         # self.INITIAL_STATE = np.random.randint(0,self.n_states)   
         self.traj                            = []
         self.database                        = {}